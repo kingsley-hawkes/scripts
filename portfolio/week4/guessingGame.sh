aa8dbc5c89b1ee006111e9aa779b19527ee988e6
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-15 13:40:26 +0000

    Pushed changes made to guessingGame and ipinfo scripts for week4.

--- portfolio/week4/guessingGame.sh
+++ portfolio/week4/guessingGame.sh
@@ -27,20 +27,53 @@ guessNumber ()
         read -p "$1: " 
       
     done 
+}
 
-	if [ $1 = "42" ]; then
-                echo "Correct!"
-                exit 0
-        elif [ $1 < "42" ]; then
-                echo "Too low!"
-        elif [ $1 > "42" ]; then
-                echo "Too high!"
-	fi
 
-      
-} 
-      
-     
-echo -e "This is a guessing game!\nTry to guess the secret number!?" 
-      
-guessNumber "Please type a number between 1 and 100" 1 100 
+# Main script
+ while [[ $REPLY -ne 42 ]]; do
+	
+
+guessNumber "please type a number between 1 and 100" 1 100
+	
+
+    if [[ $REPLY -eq 42 ]]; then
+	
+
+        echo "That's right!"
+	
+
+    elif [[ $REPLY -gt 42 ]]; then
+	
+
+        echo "Too high!"
+	
+
+    else
+	
+
+        echo "Too low!"
+	
+
+    fi
+	
+
+done 
+
+# Trail script
+#while [ $1 -ne 42 ]; do
+#	echo -e "This is a guessing game!\nTry to guess the secret number!?"
+#	guessNumber "Please type a number between 1 and 100" 1 100
+#
+#	if [ $1 -eq 42 ]; then
+#		echo "That's right!"
+#       elif [ $1 -lt 42 ]; then
+#                echo "Too low!"
+#        elif [ $REPLY -gt 42 ]; then
+#                echo "Too high!"
+#	fi
+#
+#done
+
+echo 'Thank you!"
+exit 0
--- portfolio/week4/ipinfo.sh
+++ portfolio/week4/ipinfo.sh
@@ -1,5 +1,4 @@
-
-       #!/bin/bash 
+#!/bin/bash 
       
 #get info about networking from the ifconfig command 
       
