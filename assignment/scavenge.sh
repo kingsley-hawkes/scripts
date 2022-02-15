6dea93a8f00042549d221cc1a042edcdc21ea7f5
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-15 14:28:24 +0000

    Adding small adjustments to my scavenge script to get rid of unwanted text.

--- assignment/scavenge.sh
+++ assignment/scavenge.sh
@@ -2,4 +2,7 @@
 
 #Scavenge web page and puts it into a text file.
 
-curl -o scavenge.txt https://www.cyber.gov.au/acsc/view-all-content/alerts&advisories
+curl 'https://www.cyber.gov.au/acsc/view-all-content/alerts&advisories' | 
+html2text | 
+sed -n '/12_Feb_2022_-_Alert_status:_CRITICAL/,$p' |
+sed -n '/Pagination/q;p' > scavenge.txt
