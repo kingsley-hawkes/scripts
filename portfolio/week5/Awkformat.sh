69a5725d70787cf74c7dd3eed11195a4b30169ac
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-17 14:48:57 +0000

    Tidied up Table in Awkformat.sh

--- portfolio/week5/Awkformat.sh
+++ portfolio/week5/Awkformat.sh
@@ -2,9 +2,25 @@
       
 echo "Google Server IPs:" 
       
-awk '{ 
+awk 'BEGIN { 
       
-    print $1 
+    FS=":"; 
+      
+    print "________________________________"; 
+      
+    print "| \033[34mServer Type\033[0m | \033[34mIP             \033[0m |"; 
+      
+} 
+      
+{ 
+      
+    printf("| \033[33m%-11s\033[0m | \033[35m%-15s\033[0m |\n", $1, $2); 
+      
+} 
+      
+END { 
+      
+    print("________________________________"); 
       
 }' input.txt 
      
