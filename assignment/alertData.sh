bb612a9c2d648f221795de9720a7d25912c567c2
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-21 13:02:37 +0000

    Updated scripts for scavenging and tabling data so that I can seperate year and month into two different columns for analysis purposes.

--- assignment/alertData.sh
+++ assignment/alertData.sh
@@ -5,9 +5,10 @@ awk '
 
 BEGIN {
 	FS=":";
-	print "Date        |Alert Status|Description";
+	print "| Year | Month |Alert Status|";
+	print "|------|-------|------------|";
 }
 #Match any date in DD MMM YYYY format and print date and alert status in a table.
-/[0-9]{2} [A-Z][a-z]{2} [0-9]{4}/ {
-	printf("|%-11s|%-12s|%s\n", $1, $3, $5);
+/[0-9]{2}:[A-Z][a-z]{2}:[0-9]{4}/ {
+	printf("|%-6s|%-7s|%-12s|\n", $3, $2, $8);
 }' scavenge.txt > data.txt
--- assignment/scavenge.sh
+++ assignment/scavenge.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 #Scavenge web page and puts it into a text file.
-
 curl 'https://www.cyber.gov.au/acsc/view-all-content/alerts&advisories' | 
 html2text | 
 sed -n '/12_Feb_2022_-_Alert_status:_CRITICAL/,$p' |
 sed -n '/Pagination/q;p' |
-sed 's/_/ /g; s/ - /: /g' > scavenge.txt
+sed 's/_/:/g; s/ - /: /g' > scavenge.txt
+
+curl "https://www.cyber.gov.au/acsc/view-all-content/alerts%26advisories?title_op=word&title=&body_value_op=word&body_value=&sort_by=field_date_user_updated_value&sort_order=DESC&page=1" |
+html2text |
+sed -n '/\[Search\] \[Clear search\]/,$p' |
+sed -n '/Pagination/q;p' |
+sed 's/_/:/g; s/ - /: /g' >> scavenge.txt
+
+curl "https://www.cyber.gov.au/acsc/view-all-content/alerts%26advisories?title_op=word&title=&body_value_op=word&body_value=&sort_by=field_date_user_updated_value&sort_order=DESC&page=2" |
+html2text |
+sed -n '/\[Search\] \[Clear search\]/,$p' |
+sed -n '/Pagination/q;p' |
+sed 's/_/:/g; s/ - /: /g' >> scavenge.txt
+
+
+
 
 
 # code addapted from t. Mester (2020).
