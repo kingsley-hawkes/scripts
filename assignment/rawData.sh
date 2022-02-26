b75b2637033c376520d09607e0c77dee1ddf0787
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-26 09:56:18 +0000

    Pushing rawData.sh script

---
+++ assignment/rawData.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Seperating Alert status from descriptions.
+awk '
+
+BEGIN {
+	FS=":";
+}
+
+#Match any date in DD MMM YYYY format and print date and alert status in a table.
+/[0-9]{2}:[A-Z][a-z]{2}:[0-9]{4}/ {
+	printf("%-6s %-7s %-12s\n", $3, $2, $8);
+}' scavenge.txt > rawData.txt
