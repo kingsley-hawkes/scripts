0975ff6f2034d8ff1cc098022ff7a471e879a2b7
Ubuntu <student@ML-RefVm-511909.n3x0sr3pk1iuxdpriuee2i43ae.px.internal.cloudapp.net>
2022-02-26 02:38:01 +0000

    Added another adjustment to regexes.sh script in relation to capturing passwords.

--- portfolio/week5/regexes.sh
+++ portfolio/week5/regexes.sh
@@ -10,4 +10,10 @@ grep -r -E '/d{3}'
 
 grep -r -e '"echo",/s,/w+,/s,/w+,/s,/w+,?.*'
 
-grep -r -E '/d && /[A-Z] && /[a-z] && [[:punct:]]' 
+grep -r -E '/d && /[A-Z] && /[a-z] && [[:punct:]]'
+#Code for matching a password was by M. Mombrea (2013). 
+
+
+#Mombrea, Matthew. (2013, June 11). How to validate password strength using a regular expression. Computerworld. 
+#https://www.computerworld.com/article/2833081/how-to-validate-password-strength-using-a-regular-expression.html
+
